d599bfcccda894e54c15de4b5b937218941235f3
Helio Rosado Neto <rosadohelio@hotmail.com>
2024-04-16 22:32:54 -0300

    final project

--- src/controller/homeControler.ts
+++ src/controller/homeControler.ts
@@ -1,14 +1,61 @@
-import {Request, Response} from "express"
+import { Request, Response } from "express"
+import { Pet } from "../model/pet";
+import { createMenuObject } from "../helpers/CreateMenuObject";
+interface Banner {
+   title: string;
+   background: string;
+}
 
-export const home = (req: Request, res:Response)=>{
-   res.send('home')
+export const home = (req: Request, res: Response) => {
+   let list = Pet.getAll();
+   const banner: Banner = {
+      title: 'Todos os animais',
+      background: 'allanimals.jpg'
+   }
+
+   res.render('pages/home', {
+      banner,
+      menu: createMenuObject('all'),
+      list
+   })
 }
-export const dogs = (req: Request, res:Response)=>{
-   res.send('dogs')
+
+export const dogs = (req: Request, res: Response) => {
+   let list = Pet.getFromType('dog')
+   const banner: Banner = {
+      title: 'Cachorros',
+      background: 'banner_dog.jpg'
+   }
+
+   res.render('pages/home', {
+      banner,
+      menu: createMenuObject('dog'),
+      list
+   })
 }
-export const cats = (req: Request, res:Response)=>{
-   res.send('cats')
+export const cats = (req: Request, res: Response) => {
+   let list = Pet.getFromType('cat')
+   const banner: Banner = {
+      title: 'Gatos',
+      background: 'banner_cat.jpg'
+   }
+
+   res.render('pages/home', { 
+      banner, 
+      menu: createMenuObject('cat'),
+      list
+   })
 }
-export const fishes = (req: Request, res:Response)=>{
-   res.send('fishes')
+export const fishes = (req: Request, res: Response) => {
+   let list = Pet.getFromType('fish');
+   const banner: Banner = {
+      title: 'Peixes',
+      background: 'banner_fish.jpg'
+   }
+
+   res.render('pages/home', { 
+      banner, 
+      menu: createMenuObject('fish'),
+      list
+   })
 }
--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -1,5 +1,17 @@
 import {Request, Response} from "express"
-
+import { Pet } from "../model/pet"
+import { createMenuObject } from "../helpers/CreateMenuObject"
 export const search = (req: Request, res:Response)=>{
-   res.send('search')
+   let query: string = req.query.q as string;
+   if(!query){
+      res.redirect('/');
+      return;
+   }
+   let list = Pet.getFromName(query);
+   res.render('pages/home',{
+      menu: createMenuObject(''),
+      list,
+      query
+   })
+  
 }
